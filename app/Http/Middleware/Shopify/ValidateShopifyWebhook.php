4de1d5718237673d2e9ee41669422a01ff372234
Cameron Peace <cpeace@phone2action.com>
2021-06-23 11:41:36 -0400

    removed useless comments

--- app/Http/Middleware/Shopify/ValidateShopifyWebhook.php
+++ app/Http/Middleware/Shopify/ValidateShopifyWebhook.php
@@ -22,21 +22,19 @@ class ValidateShopifyWebhook
         }
 
         // if(!$request->secure()) {
-        //     \Log::info('insecure');
+        //       //we won't be redirecting. TODO
         //     return redirect()->secure($request->getRequestUri());
         // }
 
         $signature = $request->header('x-shopify-hmac-sha256');
 
         if(!isset($signature)) {
-            \Log::info('no sig');
-            return response('Forbidden', 403);
+\            return response('Forbidden', 403);
         }
         
         $hmac = base64_encode(hash_hmac('sha256', $request->getContent(), config('services.shopify.sign'), true));
 
         if(!hash_equals($signature, $hmac)) {
-            \Log::info('no match');
             return response('Forbidden: Invalid signature', 403);
         }
 
